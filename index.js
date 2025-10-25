659aa631041022d589957cdd21c9c32d3d901a8a
ilyosbekn1n1@gmail.com <ilyosbekn1n1@gmail.com>
2025-10-25 16:19:38 +0000

    tgbot

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const TOKEN = "8484604597:AAFUKp5va185-YQY4jbxxMZXPCYXkERYOBQ";
+
+const bot = new TelegramBot(TOKEN, {polling: true});
+bot.on("massage", function(msg) {
+    const chatId = msg.chat.Id;
+    const text = msg.text;
+
+    bot.sendMessage(chatId, 'salom');
+    
+    console.log(msg);
+    
+})
+
+
 console.log(salom);
+
